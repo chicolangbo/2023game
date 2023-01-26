3c47ceafc9d0be6521a977debc849b4ad7322649
minji4885 <minji4885@gmail.com>
2023-01-27 00:31:39 +0900

    calculator_answer

--- calculator.c
+++ calculator.c
@@ -1,18 +1,24 @@
-// 사칙연산 프로그램 만들기
-#include<stdio.h>
-int main(void)
-{
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
   int a, b;
+  int c, d, e;
+  float f;
   printf("input 2 data:");
-  scanf("%d %d",&a, &b);
-  printf("%d + %d = %d\n", a, b, a+b);
-  printf("%d - %d = %d\n", a, b, a-b);
-  printf("%d * %d = %d\n", a, b, a*b);
-  printf("%d / %d = %d\n", a, b, a/b);
-  if(a>=b){
-   printf("max = %d", a);
-  }
-  else{
+  scanf("%d %d", &a, &b);
+  c = a + b;
+  d = a - b;
+  e = a * b;
+  f = (float)a / b;
+
+  printf("%d + %d = %d\n", a, b, c);
+  printf("%d - %d = %d\n", a, b, d);
+  printf("%d * %d = %d\n", a, b, e);
+  printf("%d / %d = %.2f\n", a, b, f);
+  if (a >= b) {
+    printf("max = %d", a);
+  } else {
     printf("max = %d", b);
   }
   return 0;
