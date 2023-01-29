0ce9b8085e66729687411e9ccb43b70e89b72c1a
minji4885 <minji4885@gmail.com>
2023-01-29 22:44:39 +0900

    lotto_answer

---
+++ lotto.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+  int num[6];
+  int i;
+  int j;
+  srand(time(NULL));
+
+  for(i=0;i<6;i++){
+    num[i] = rand()%46 + 1;
+    for(j=0;j<i;j++){
+      if(num[i] == num[j]){
+        i--;
+        break;
+      }
+    }
+  }
+
+  for(i=0;i<6;i++){
+    printf("%d\n", num[i]);
+  }
+  
+  return 0;
+  }
